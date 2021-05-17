27056b59b4a912c427de437b7fdac091f08db499
itsjenny <48797215+itsjenny@users.noreply.github.com>
2021-05-17 19:07:04 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component } from "react";
+import CollectFilters from "./comp/CollectFilters";
+
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <div className="App">
+        <CollectFilters/>
+      </div>
+    );
+  }
+}
+
+export default App;
+ 
